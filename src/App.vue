c04ea9d8ee3346c4e8aef18db72b6e8e60adc70d
BraindeadDogge <grigory.almaznickov@gmail.com>
2019-12-21 03:28:17 +0300

    Now UserCard component gets data from props

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    
+    <Usercard></Usercard>
   </div>
 </template>
 
 <script>
+import Usercard from "./components/Usercard.vue"
+
 export default {
   name: 'app',
   data () {
     return {
-      
+      Usercard
     }
   }
 }
---
+++ src/components/Usercard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="user-card">
+    <img :src="url" class="user-photo">
+    <p class="user-nickname">{{login}}</p>
+    <p class="user-name">
+        {{lastname}} {{name}}<br>
+        {{extraname}}
+    </p>
+    <hr>
+    <p class="user-info-item">
+        <img src="img/location.png">
+        {{adress}}
+    </p>
+    <p class="user-info-item">
+        <img src="img/mail.png">
+        {{mail}}
+    </p>
+    <p class="user-info-item">
+        <img src="img/phone.png">
+        {{phone}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Usercard',
+    props: ['url', 'login', 'name', 'secondname', 'extraname', 'adress', 'mail', 'phone', ],
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
